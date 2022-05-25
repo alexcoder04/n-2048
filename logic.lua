ee6b0dd500775f3638bf088436669a73f80986df
alexcoder04 <alexcoder04@protonmail.com>
2022-05-25 23:05:39 +0200

    game end; add twos only if is free field

--- logic.lua
+++ logic.lua
@@ -40,6 +40,37 @@ function mergeLeft(grid)
     return grid
 end
 
+function areFreeFields(grid)
+    for row = 1, 4 do
+        for col = 1, 4 do
+            if grid[row][col] == 0 then
+                return true
+            end
+        end
+    end
+    return false
+end
+
+function gameLost(grid)
+    if areFreeFields(grid) then return false end
+    for row = 1, 4 do
+        for cell = 1, 3 do
+            if(grid[row][cell] == new[row][cell+1] and grid[row][cell] ~= 0) then
+                return false
+            end
+        end
+    end
+    grid = rotateClockwise(grid)
+    for row = 1, 4 do
+        for cell = 1, 3 do
+            if(grid[row][cell] == new[row][cell+1] and grid[row][cell] ~= 0) then
+                return false
+            end
+        end
+    end
+    return true
+end
+
 function rotateClockwise(f)
     n_f = {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}}
     for i=1, 4 do
@@ -61,6 +92,7 @@ function mirrorHoriz(f)
 end
 
 function on.charIn(char)
+    if gameOver then return end
     if char == '8' then
         field = rotateClockwise(field)
         field = moveLeft(field)
@@ -69,9 +101,15 @@ function on.charIn(char)
         field = rotateClockwise(field)
         field = rotateClockwise(field)
         field = rotateClockwise(field)
-        field = addTwo(field)
+        if gameLost(field) then
+            gameOver = true
+            platform.window:invalidate()
+            return
+        end
+        if areFreeFields(field) then
+            field = addTwo(field)
+        end
         platform.window:invalidate()
-        return
     end
     if char == '2' then
         field = rotateClockwise(field)
@@ -83,17 +121,29 @@ function on.charIn(char)
         field = rotateClockwise(field)
         field = rotateClockwise(field)
         field = rotateClockwise(field)
-        field = addTwo(field)
+        if gameLost(field) then
+            gameOver = true
+            platform.window:invalidate()
+            return
+        end
+        if areFreeFields(field) then
+            field = addTwo(field)
+        end
         platform.window:invalidate()
-        return
     end
     if char == '4' then
         field = moveLeft(field)
         field = mergeLeft(field)
         field = moveLeft(field)
-        field = addTwo(field)
+        if gameLost(field) then
+            gameOver = true
+            platform.window:invalidate()
+            return
+        end
+        if areFreeFields(field) then
+            field = addTwo(field)
+        end
         platform.window:invalidate()
-        return
     end
     if char == '6' then
         field = mirrorHoriz(field)
@@ -101,9 +151,15 @@ function on.charIn(char)
         field = mergeLeft(field)
         field = moveLeft(field)
         field = mirrorHoriz(field)
-        field = addTwo(field)
+        if gameLost(field) then
+            gameOver = true
+            platform.window:invalidate()
+            return
+        end
+        if areFreeFields(field) then
+            field = addTwo(field)
+        end
         platform.window:invalidate()
-        return
     end
 end
 
--- main.lua
+++ main.lua
@@ -6,8 +6,13 @@ field = {
     {0, 0, 0, 0}
 }
 
+gameOver = false
+
 function on.paint(gc)
     drawCells(gc)
     drawNumbers(gc)
+    if gameOver then
+        gc:drawString("YOU LOST", 50, 50, "middle");
+    end
 end
 
