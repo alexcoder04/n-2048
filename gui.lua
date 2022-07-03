e657be0ccbf992e95d3c7f708a99d9711f6a1eb3
alexcoder04 <alexcoder04@protonmail.com>
2022-07-03 22:42:12 +0200

    colors

--- gui.lua
+++ gui.lua
@@ -3,6 +3,22 @@ cellWidth = 40
 marginLeft = 80
 marginTop = 25
 
+colors = {
+    {245, 234, 221},
+    {245, 230, 205},
+    {245, 183, 124},
+    {245, 157, 86},
+    {15, 130, 98},
+    {245, 94, 60},
+    {245, 213, 118},
+    {245, 211, 101},
+    {245, 204, 84},
+    {245, 206, 71},
+    {245, 200, 49},
+    {245, 67, 64},
+    {74, 64, 54}
+}
+
 function drawCells(gc)
     gc:drawLine(marginLeft, marginTop, marginLeft, marginTop+(cellWidth*4))
     for i=1, 4 do
@@ -32,9 +48,30 @@ function getStringFromExponent(exponent)
     return tostring(2^exponent)
 end
 
+function drawColors(gc)
+    for i=1, 4 do
+        for j=1, 4 do
+            if field[i][j] ~= 0 then
+                gc:setColorRGB(unpack(colors[field[i][j]]))
+                gc:fillRect(
+                    marginLeft+(j*cellWidth)-cellWidth+1,
+                    marginTop+(i*cellWidth)-cellWidth+1,
+                    cellWidth-1,
+                    cellWidth-1
+                )
+            end
+        end
+    end
+end
+
 function drawNumbers(gc)
     for i=1, 4 do
         for j=1, 4 do
+            if field[i][j] == 1 or field[i][j] == 2 then
+                gc:setColorRGB(144, 107, 124)
+            else
+                gc:setColorRGB(255, 246, 255)
+            end
             gc:drawString(
                 getStringFromExponent(field[i][j]),
                 marginLeft+((j-1)*cellWidth)+(cellWidth/2)-(#getStringFromExponent(field[i][j])*4),
--- main.lua
+++ main.lua
@@ -10,6 +10,7 @@ gameOver = false
 
 function on.paint(gc)
     drawCells(gc)
+    drawColors(gc)
     drawNumbers(gc)
     if gameOver then
         gc:drawString("YOU LOST!", 130, 15, "middle");
